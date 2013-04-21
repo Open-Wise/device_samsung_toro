a055b7dfeac807248b7821ac3882f23b47e8c907
franco-c <tjrdnjs33936@gmail.com>
2013-04-21 17:28:30 +0900

    MIRAGEFY
    Change-Id: Ib8794b0b1755768f7735151e4179fd35ffab6cf2

--- slim.mk
+++ slim.mk
@@ -5,16 +5,16 @@ TARGET_BOOTANIMATION_NAME := vertical-720x1280
 $(call inherit-product, device/samsung/toro/full_toro.mk)
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+$(call inherit-product, vendor/mirage/config/common_full_phone.mk)
 
 # Inherit device settings
-$(call inherit-product, vendor/slim/config/common_nexus.mk)
+$(call inherit-product, vendor/mirage/config/common_nexus.mk)
 
 # Inherit torch settings
-$(call inherit-product, vendor/slim/config/common_ledflash.mk)
+$(call inherit-product, vendor/mirage/config/common_ledflash.mk)
 
 PRODUCT_COPY_FILES +=  \
-    vendor/slim/prebuilt/xhdpi/bootanimation.zip:system/media/bootanimation.zip
+    vendor/mirage/prebuilt/xhdpi/bootanimation.zip:system/media/bootanimation.zip
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.camera.res=5MP
@@ -25,7 +25,7 @@ PRODUCT_RELEASE_NAME := toro
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mysid BUILD_FINGERPRINT="google/mysid/toro:4.2.2/JDQ39/125147:user/release-keys"
 PRIVATE_BUILD_DESC="mysid-user 4.2.2 JDQ39 125147 release-keys"
 
-PRODUCT_NAME := slim_toro
+PRODUCT_NAME := mirage_toro
 PRODUCT_DEVICE := toro
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Galaxy Nexus
